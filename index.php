f90790cf84445439826d0daada095e20beeabe61
LIN YITUNG <a111221065@mail.shu.edu.tw>
2025-08-11 22:07:21 +0800

    chore: add index page for API entry

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<h2>cafe_api</h2>
+<ul>
+  <li><a href="/get_cafes.php">get_cafes.php</a></li>
+  <li><a href="/get_cafes_by_location.php?lat=25.04&lng=121.56&radius=3000">get_cafes_by_location.php</a></li>
+  <li><a href="/search_cafes.php?q=wifi">search_cafes.php</a></li>
+  <li><a href="/generate_itinerary.php?day=1">generate_itinerary.php</a></li>
+</ul>
